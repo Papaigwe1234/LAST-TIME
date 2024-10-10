1aa4a0f091207d40b169c2ed64eb1db5af4a3668
PAPA IGWE <papaigwe0@gmail.com>
2024-10-10 01:35:22 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+//base by BIG DADDY (Phistar Bot Inc.)
+//YouTube: @Phistar
+//Instagram: @Phistar
+//Telegram: @BIG DADDY 
+//GitHub: @Phistar
+//WhatsApp: +23428019141
+//want more free bot scripts? subscribe to my youtube channel: https://youtube.com/@phistar
+
+const {
+   spawn
+} = require('child_process')
+const path = require('path')
+
+function start() {
+   let args = [path.join(__dirname, 'main.js'), ...process.argv.slice(2)]
+   console.log([process.argv[0], ...args].join('\n'))
+   let p = spawn(process.argv[0], args, {
+         stdio: ['inherit', 'inherit', 'inherit', 'ipc']
+      })
+      .on('message', data => {
+         if (data == 'reset') {
+            console.log('Restarting Bot...')
+            p.kill()
+            start()
+            delete p
+         }
+      })
+      .on('exit', code => {
+         console.error('Exited with code:', code)
+         if (code == '.' || code == 1 || code == 0) start()
+      })
+}
+start()
